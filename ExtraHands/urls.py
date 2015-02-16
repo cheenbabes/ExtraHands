bc539b6066c96d6f76c6870908896823768e2a65
Eugene Baibourine <eugene.baibourin@gmail.com>
2015-02-16 16:47:02 -0700

    View working with url routing!

--- ExtraHands/urls.py
+++ ExtraHands/urls.py
@@ -7,4 +7,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', include('extra_hands_app.urls')),
 )
+
---
+++ extra_hands_app/urls.py
@@ -0,0 +1,5 @@
+from django.conf.urls import patterns, url
+from extra_hands_app import views
+
+urlpatterns = patterns('',
+url(r'^$', views.index, name='index'))
